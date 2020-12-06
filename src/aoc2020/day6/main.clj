f2ebdf21fe4fcf1d0b42149f80b743f9bbe2270e
Louis Clergue <louis.clergue@king.com>
2020-12-06 09:28:04 +0100

    Solve day6

---
+++ src/aoc2020/day6/main.clj
@@ -0,0 +1,28 @@
+(ns aoc2020.day6.main
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(def input (->> (io/resource "input-06.txt")
+                (io/reader)
+                (slurp)))
+
+(defn group-count [group]
+  (count (set (str/replace group #"\R" ""))))
+
+(->> (str/split input #"\R\R")
+     (map group-count)
+     (reduce +))
+;; => 6782
+
+
+(defn group-count2 [group]
+  (let [groups (str/split group #"\n")]
+    (->> groups
+         (map set)
+         (reduce clojure.set/intersection)
+         (count))))
+
+(->> (str/split input #"\R\R")
+     (map group-count2)
+     (reduce +))
+;; => 3596
